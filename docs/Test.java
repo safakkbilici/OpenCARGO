b216997e076bb5a57b41489133dc735e17d21096
Semih Şenel <59144716+semihsenel@users.noreply.github.com>
2020-04-29 20:08:01 +0300

    Add files via upload

---
+++ docs/Test.java
@@ -0,0 +1,14 @@
+
+package kargo;
+
+
+public class Test {
+    
+    
+    public static void main(String[] args) {
+        Arayuz arayuz = new Arayuz();
+        arayuz.setVisible(true);
+        
+    }
+    
+}
